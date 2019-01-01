41cabbd9d2d85f2851a301e56885e09e7dd8c038
hoparkgo9ma <me@ho9.me>
2019-01-01 23:17:39 +0900

    환경 변수 추가

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ class EverythingConverter(werkzeug.routing.PathConverter):
 
 app.jinja_env.filters['md5_replace'] = md5_replace
 app.jinja_env.filters['load_lang'] = load_lang
+app.jinja_env.filters['load_langs'] = load_langs
 
 app.url_map.converters['everything'] = EverythingConverter
 
